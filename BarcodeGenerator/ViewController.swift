4de6d63d183c8da865e3dc53c2bbbcc27ee6848b
Kylie <chiangkaiyun@gmail.com>
2021-06-02 21:22:16 +0800

    Add variable init

--- BarcodeGenerator/ViewController.swift
+++ BarcodeGenerator/ViewController.swift
@@ -9,18 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var codeTextField: UITextField!
-    @IBOutlet weak var lengthTextField: UITextField!
+    @IBOutlet weak var heightTextField: UITextField!
     @IBOutlet weak var widthTextField: UITextField!
     @IBOutlet weak var quietSpaceTextField: UITextField!
     @IBOutlet weak var barcodeImageView: UIImageView!
-    
+    @IBOutlet weak var barcodeImageWidthConstraint: NSLayoutConstraint!
+    @IBOutlet weak var barcodeImageHeightConstraint: NSLayoutConstraint!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initBarcode()
     }
 
     @IBAction func tappedRefreshButton(_ sender: Any) {
+        initBarcode()
     }
     
+    func initBarcode() {
+        guard let code = codeTextField.text else { return }
+        guard let space = quietSpaceTextField.text, space != "" || space != "0" else { return }
+        guard let width = widthTextField.text, width != "" || width != "0" else { return }
+        guard let height = heightTextField.text, height != "" || height != "0" else { return }
+        guard let barcodeQuietSpace = Double(space) else { return }
+        
+        barcodeImageWidthConstraint.constant = CGFloat(Double(width) ?? 0.0)
+        barcodeImageHeightConstraint.constant = CGFloat(Double(height) ?? 0.0)
+        
+    }
 }
 
