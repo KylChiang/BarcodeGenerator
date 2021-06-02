0748770dedca7e937fc0aa4008868f422c4ad325
Kylie <chiangkaiyun@gmail.com>
2021-06-02 19:29:27 +0800

    Add layout & IB functions

--- BarcodeGenerator/ViewController.swift
+++ BarcodeGenerator/ViewController.swift
@@ -8,12 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var codeTextField: UITextField!
+    @IBOutlet weak var lengthTextField: UITextField!
+    @IBOutlet weak var widthTextField: UITextField!
+    @IBOutlet weak var quietSpaceTextField: UITextField!
+    @IBOutlet weak var barcodeImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func tappedRefreshButton(_ sender: Any) {
+    }
+    
 }
 
